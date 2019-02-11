c2c237fefab33ad877401e4577991202945f8440
lilkho <mkhondker@stuy.edu>
2019-02-10 22:57:47 -0500

    adding a bunch of cases

--- Driver.java
+++ Driver.java
@@ -1,11 +1,23 @@
 public class Driver {
   public static void main(String[] args) {
+    QueenBoard board0 = new QueenBoard(3);
+    board0.solve();
+    System.out.println(board0);
     QueenBoard board = new QueenBoard(4);
     System.out.println(board);
     board.solve();
     System.out.println(board);
-    QueenBoard board2 = new QueenBoard(8);
+    QueenBoard board2 = new QueenBoard(5);
     board2.solve();
     System.out.println(board2);
+    QueenBoard board3 = new QueenBoard(6);
+    board3.solve();
+    System.out.println(board3);
+    QueenBoard board4 = new QueenBoard(7);
+    board4.solve();
+    System.out.println(board4);
+    QueenBoard board5 = new QueenBoard(8);
+    board5.solve();
+    System.out.println(board5);
   }
 }
