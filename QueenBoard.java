d19b06b189210dc5cc9409b04ae07d185b5c83fc
lilkho <mkhondker@stuy.edu>
2019-02-10 22:47:52 -0500

    changing back tostring

--- QueenBoard.java
+++ QueenBoard.java
@@ -52,7 +52,7 @@ public class QueenBoard {
     String s = "";
     for (int i=0;i<board.length;i++) {
       for (int j=0;j<board.length;j++) {
-        if (board[i][j]>=0) s+=board[i][j]+" ";
+        if (board[i][j]>=0) s+="_ ";
         if (board[i][j]==-1) s+="Q ";
       }
       s+="\n";
